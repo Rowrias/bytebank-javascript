8e81c4ca8ea4e0eedad0fe1214c8cdbc3911d5c6
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-12-19 10:26:42 -0300

    1.1

---
+++ script/scripts.js
@@ -0,0 +1,21 @@
+const graficoDolar = document.getElementById('graficoDolar')
+
+const graficoParaDolar = new Chart(graficoDolar, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+});
+
+async function conectaAPI() {
+    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/:moedas")
+    const conectaTraduzido = await conecta.json();
+    console.log(conectaTraduzido);    
+}
+
+conectaAPI();
