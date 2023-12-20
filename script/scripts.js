de22fba4ad624b77325dc823dd0e41c422117d81
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-12-20 09:09:17 -0300

    4.1

--- script/scripts.js
+++ script/scripts.js
@@ -14,17 +14,6 @@ const graficoParaDolar = new Chart(graficoDolar, {
     },
 });
 
-setInterval(() => conectaAPI(), 5000);
-
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorario();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao('dolar', valor);
-}
-
 function geraHorario() {
     let data = new Date();
     let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
@@ -42,3 +31,10 @@ function adicionarDados(grafico, legenda, dados) {
 
 let workerDolar = new Worker('./script/workers/workerDolar.js');
 workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
