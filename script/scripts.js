a4f2a5bee1715faef4f85d1725f369bd70074bb7
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-12-20 08:55:19 -0300

    3.1

--- script/scripts.js
+++ script/scripts.js
@@ -1,23 +1,28 @@
+import imprimeCotacao from "./imprimeCotacao.js";
+
 const graficoDolar = document.getElementById('graficoDolar')
 
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1
         }]
     },
 });
 
-setInterval(() => conectaAPI(), 10000);
+setInterval(() => conectaAPI(), 5000);
 
 async function conectaAPI() {
     const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
     const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);    
+    let tempo = geraHorario();
+    let valor = conectaTraduzido.USDBRL.ask;
+    adicionarDados(graficoParaDolar, tempo, valor);
+    imprimeCotacao('dolar', valor);
 }
 
 function geraHorario() {
@@ -27,4 +32,13 @@ function geraHorario() {
     return horario;
 }
 
-geraHorario();
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda)
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    })
+    grafico.update();
+}
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
